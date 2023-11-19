801f701a0467dcb1606d1644ad4ededdb175df45
Pedro Pastuk <pastukpedro@gmail.com>
2023-11-19 00:08:10 -0300

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactamplifyv2302b9c33": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+function App() {
+	return (
+		<Authenticator socialProviders={['google']}>
+			{({ signOut, user }) => (
+				<main>
+					<h1>Hello {user.username}</h1>
+					<button onClick={signOut}>Sign out</button>
+				</main>
+			)}
+		</Authenticator>
+	);
+}
+
+export default App;
